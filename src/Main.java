161f883ca8474455a651c03ec822967443de4897
DJEDOU KODJO <tauny225@gmail.com>
2022-06-25 04:52:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int Age = 25;
+
+        if (Age >= 21)
+            {
+                System.out.println("You are eligible to receive a wrist band");
+            }
+
+        // write your code here
+    }
+}
